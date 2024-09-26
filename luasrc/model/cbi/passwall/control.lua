7bd42c68614c8b60e013166da26ac1105773825d
lm317379829 <lm317379829@163.com>
2024-09-26 15:00:13 +0800

    Update control.lua

--- luasrc/model/cbi/passwall/control.lua
+++ luasrc/model/cbi/passwall/control.lua
@@ -1,14 +1,14 @@
 m = Map("passwall")
 s = m:section(TypedSection, "passwall")
 
--- 在控制面板中显示 sing-box 的运行状态
+-- 在控制面板中显示 passwall 的运行状态
 status = s:option(DummyValue, "status", translate("状态"))
 status.rawhtml = true
 status.cfgvalue = function(self, section)
     return getSingBoxStatus()
 end
 
--- 在控制面板中控制 sing-box 的运行
+-- 在控制面板中控制 passwall 的运行
 o = s:option(ListValue, "control", "控制")
 
 -- 定义选项的值
@@ -45,9 +45,9 @@ function restartPassWall()
     luci.http.redirect(luci.dispatcher.build_url("admin", "services", "passwall"))
 end
 
--- 添加检查 sing-box 运行状态的函数
+-- 添加检查 passwall 运行状态的函数
 function getSingBoxStatus()
-    local running = luci.sys.call("pgrep sing-box >/dev/null") == 0
+    local running = luci.sys.call("pgrep passwall >/dev/null") == 0
     if running then
         return "<span style='color: green;'><strong>Sing-Box 正在运行</strong></span>"
     else
