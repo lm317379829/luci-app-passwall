8c7adac9abda35f5ccb4993438b727ebccd30eba
lm317379829 <lm317379829@163.com>
2024-11-01 15:19:50 +0800

    Update config.lua

--- luasrc/model/cbi/passwall/config.lua
+++ luasrc/model/cbi/passwall/config.lua
@@ -1,10 +1,14 @@
-m = Map("passwall")
-s = m:section(TypedSection, "passwall")
 local fs = require "nixio.fs"
+local Map, Section, Value = require("luci.model.uci").cursor(), Map, Value
+
+m = Map("passwall", _("Passwall Configuration"))
+s = m:section(TypedSection, "passwall", _("Configuration Settings"))
+s.anonymous = true
+
 local filePath = "/usr/local/etc/passwall/config.json"
 local dirPath = "/usr/local/etc/passwall"
 
--- 检查文件是否存在，如果目录不存在则创建目录，如果文件不存在则创建空文件
+-- 检查文件和目录是否存在
 if not fs.stat(dirPath) then
     fs.mkdirr(dirPath)  -- 创建目录
 end
@@ -13,17 +17,20 @@ if not fs.stat(filePath) then
     fs.writefile(filePath, "")  -- 创建空白文件
 end
 
-configTemplate = s:option(Value, "configTemplate")
+-- 配置模板
+configTemplate = s:option(Value, "configTemplate", _("Configuration Template"))
 configTemplate.template = "cbi/tvalue"
 configTemplate.rows = 20
 
+-- 读取配置文件内容
 function configTemplate.cfgvalue(self, section)
-    return fs.readfile(filePath)
+    return fs.readfile(filePath) or ""
 end
 
+-- 写入配置文件内容
 function configTemplate.write(self, section, value)
     value = value:gsub("\r\n?", "\n")
-    nixio.fs.writefile(filePath, value)
+    fs.writefile(filePath, value)
 end
 
 return m
