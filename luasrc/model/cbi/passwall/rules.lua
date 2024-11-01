7cd5e0f5696a98ba51b38a13b5d6388ef82a8c88
lm317379829 <lm317379829@163.com>
2024-11-01 15:22:30 +0800

    Update rules.lua

--- luasrc/model/cbi/passwall/rules.lua
+++ luasrc/model/cbi/passwall/rules.lua
@@ -1,4 +1,7 @@
 local fs = require("nixio.fs")
+local tpl = require("luci.template")
+local dispatcher = require("luci.dispatcher")
+
 local rulesDir = "/usr/local/etc/passwall/rules"
 
 -- 获取规则文件列表
@@ -13,7 +16,7 @@ local function getRuleFiles()
     -- 排序函数：先按长度排序，如果长度相同，按第一个字符排序
     table.sort(files, function(a, b)
         if #a == #b then
-            return a:sub(1, 1) < b:sub(1, 1)  -- 长度相同时，按第一个字符排序
+            return a < b  -- 长度相同时按字母排序
         else
             return #a < #b  -- 按长度从短到长排序
         end
@@ -22,42 +25,52 @@ local function getRuleFiles()
     return files
 end
 
+-- 描述信息
 local description = [[
 domain：匹配完整域名<br>
 domain_suffix：匹配域名后缀<br>
 domain_keyword：匹配域名关键字<br>
 ip_cidr：匹配 IP CIDR<br>
-详见：<a href="https://sing-box.sagernet.org/zh/configuration/route/rule/" target="_blank">https://sing-box.sagernet.org/zh/configuration/route/rule/</a>
+详见：<a href="https://sing-box.sagernet.org/zh/configuration/route/rule/" target="_blank">sing-box 规则配置</a>
 ]]
 
-m = Map("passwall", translate(""), translate(description))
+-- 创建 Map 对象
+m = Map("passwall", translate("Passwall Rules Configuration"), translate(description))
 local ruleFiles = getRuleFiles()
+
+-- 如果没有找到规则文件，显示提示
 if #ruleFiles == 0 then
-    local tpl = require("luci.template")
-    tpl.render_string([[<script type="text/javascript">
-        alert('未找到有效的规则文件');
-        window.location.href = ']] .. luci.dispatcher.build_url("admin", "services", "passwall") .. [[';
-    </script>]])
+    tpl.render_string([[
+        <script type="text/javascript">
+            alert('未找到有效的规则文件');
+            window.location.href = ']] .. dispatcher.build_url("admin", "services", "passwall") .. [[';
+        </script>
+    ]])
 else
-    local s = m:section(TypedSection, "passwall")
-    
-    -- 为每个文件生成一个 Tab 选项
+    -- 创建一个 TypedSection
+    local s = m:section(TypedSection, "passwall", translate("Rule Files"))
+    s.anonymous = true
+
+    -- 为每个规则文件生成一个 Tab 和编辑区域
     for _, file in ipairs(ruleFiles) do
         local filePath = rulesDir .. "/" .. file
-        local fileName = file:gsub("%.%w+$", "")
+        local fileName = file:gsub("%.%w+$", "")  -- 去除文件扩展名
+
         -- 创建 Tab
         s:tab(fileName, translate(fileName))
-        -- 创建 configTemplate 选项
-        local configTemplate = s:taboption(fileName, Value, fileName)
+
+        -- 创建编辑区域
+        local configTemplate = s:taboption(fileName, Value, fileName, translate("Edit ") .. fileName)
         configTemplate.template = "cbi/tvalue"
         configTemplate.rows = 20
-        -- 设置文件路径
         configTemplate.filePath = filePath
+
         -- 读取文件内容
         function configTemplate.cfgvalue(self, section)
             return fs.readfile(self.filePath) or ""
         end
-        -- 保存修改后的内容
+
+        -- 保存编辑的内容
         function configTemplate.write(self, section, value)
             if self.option == fileName then
                 fs.writefile(self.filePath, value)
