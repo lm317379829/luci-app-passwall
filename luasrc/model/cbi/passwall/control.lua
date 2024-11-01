693a414ae2e472eec5c1974711b0809f3bff37cb
lm317379829 <lm317379829@163.com>
2024-11-01 15:21:30 +0800

    Update control.lua

--- luasrc/model/cbi/passwall/control.lua
+++ luasrc/model/cbi/passwall/control.lua
@@ -1,34 +1,38 @@
 local fs = require "nixio.fs"
+local sys = require "luci.sys"
+local http = require "luci.http"
+local dispatcher = require "luci.dispatcher"
 local passwallPath = "/usr/local/bin/passwall"
+
+m = Map("passwall", _("Passwall Control Panel"))
+s = m:section(TypedSection, "passwall", _("Control Settings"))
+s.anonymous = true
 
 -- 检查文件是否存在
 if not fs.stat(passwallPath) then
     local tpl = require("luci.template")
-    tpl.render_string([[<script type="text/javascript">
-        alert('未找到sing-box，下载后重命名为passwall后放置与/usr/local/bin并给与权限');
-    </script>]])
+    tpl.render_string([[
+        <script type="text/javascript">
+            alert('未找到 sing-box，请下载后重命名为 passwall 放置于 /usr/local/bin，并确保有执行权限');
+        </script>
+    ]])
 end
 
-m = Map("passwall")
-s = m:section(TypedSection, "passwall")
-
--- 在控制面板中显示 passwall 的运行状态
+-- 显示 passwall 的运行状态
 status = s:option(DummyValue, "status", translate("状态"))
 status.rawhtml = true
 status.cfgvalue = function(self, section)
     return getSingBoxStatus()
 end
 
--- 在控制面板中控制 passwall 的运行
-o = s:option(ListValue, "control", "控制")
+-- 控制 passwall 的启动、停止和重启
+control = s:option(ListValue, "control", _("控制"))
+control:value("start", _("启动"))
+control:value("stop", _("停止"))
+control:value("restart", _("重启"))
 
--- 定义选项的值
-o:value("start", "启动")
-o:value("stop", "停止")
-o:value("restart", "重启")
-
--- 定义根据选项执行不同的函数
-function o.write(self, section, value)
+-- 根据选项执行不同的操作
+function control.write(self, section, value)
     if value == "start" then
         startPassWall()
     elseif value == "stop" then
@@ -37,28 +41,28 @@ function o.write(self, section, value)
         restartPassWall()
         value = "start"
     end
-    return ListValue.write(self, section, value)
+    ListValue.write(self, section, value)
 end
 
--- 定义不同选项对应的函数
+-- 定义启动、停止和重启函数
 function startPassWall()
-    luci.sys.call("/etc/init.d/passwall enable && /etc/init.d/passwall start")
-    luci.http.redirect(luci.dispatcher.build_url("admin", "services", "passwall"))
+    sys.call("/etc/init.d/passwall enable && /etc/init.d/passwall start")
+    http.redirect(dispatcher.build_url("admin", "services", "passwall"))
 end
 
 function stopPassWall()
-    luci.sys.call("/etc/init.d/passwall stop && /etc/init.d/passwall disable")
-    luci.http.redirect(luci.dispatcher.build_url("admin", "services", "passwall"))
+    sys.call("/etc/init.d/passwall stop && /etc/init.d/passwall disable")
+    http.redirect(dispatcher.build_url("admin", "services", "passwall"))
 end
 
 function restartPassWall()
-    luci.sys.call("/etc/init.d/passwall restart")
-    luci.http.redirect(luci.dispatcher.build_url("admin", "services", "passwall"))
+    sys.call("/etc/init.d/passwall restart")
+    http.redirect(dispatcher.build_url("admin", "services", "passwall"))
 end
 
--- 添加检查 passwall 运行状态的函数
+-- 检查 passwall 的运行状态
 function getSingBoxStatus()
-    local running = luci.sys.call("pgrep passwall >/dev/null") == 0
+    local running = sys.call("pgrep passwall >/dev/null") == 0
     if running then
         return "<span style='color: green;'><strong>Sing-Box 正在运行</strong></span>"
     else
