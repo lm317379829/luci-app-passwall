927f2750bc0f69f3295f2dfc6756f8e0cab727b5
lm317379829 <lm317379829@163.com>
2024-11-02 10:55:31 +0800

    Update passwall.lua

--- luasrc/controller/passwall.lua
+++ luasrc/controller/passwall.lua
@@ -5,9 +5,8 @@ function index()
         return
     end
 
-    -- 添加菜单项
-    entry({"admin", "services", "passwall"}, firstchild(), _("翻墙控制"), 10).dependent = false
-    entry({"admin", "services", "passwall", "control"}, cbi("passwall/control"), _("控制面板"), 1).leaf = true
-    entry({"admin", "services", "passwall", "config"}, cbi("passwall/config"), _("编辑配置"), 2).leaf = true
-    entry({"admin", "services", "passwall", "rules"}, cbi("passwall/rules"), _("编辑规则"), 3).leaf = true
+    entry({"admin", "services", "passwall"}, firstchild(), "翻墙控制", 10).dependent = false
+    entry({"admin", "services", "passwall", "control"}, cbi("passwall/control"), "控制面板", 1).leaf = true
+    entry({"admin", "services", "passwall", "config"}, cbi("passwall/config"), "编辑配置", 2).leaf = true
+    entry({"admin", "services", "passwall", "rules"}, cbi("passwall/rules"), "编辑规则", 3).leaf = true
 end
